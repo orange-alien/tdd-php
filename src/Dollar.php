97d8758ed7ca27255f6af016e592d5d7b3d0994a
orange-alien <6963921+orange-alien@users.noreply.github.com>
2023-11-25 03:19:47 +0000

    1_P6_Dollarクラス作成

---
+++ src/Dollar.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace src;
+
+class Dollar
+{
+    public function __construct(
+        public readonly int $amount
+    )
+    {
+    }
+
+    public function times(int $multiplier) : void
+    {
+
+    }
+}
--- test/MoneyTest.php
+++ test/MoneyTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use src\Dollar;
 
 use function PHPUnit\Framework\assertEquals;
 
