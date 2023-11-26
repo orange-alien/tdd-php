b5688308de56a2aacca00195bf21ded722a6c18f
orange-alien <6963921+orange-alien@users.noreply.github.com>
2023-11-26 05:21:54 +0000

    part1_chapter5_P31

---
+++ src/Franc.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace src;
+
+class Franc
+{
+    private $amount;
+
+    public function __construct(int $amount)
+    {
+        $this->amount = $amount;
+    }
+
+    public function times(int $multiplier) : Franc
+    {
+        return new Franc($this->amount * $multiplier);
+    }
+
+    public function equals(Franc $object) : bool
+    {
+        return $this->amount === $object->amount;
+    }
+}
--- test/MoneyTest.php
+++ test/MoneyTest.php
@@ -2,8 +2,7 @@
 
 use PHPUnit\Framework\TestCase;
 use src\Dollar;
-
-use function PHPUnit\Framework\assertEquals;
+use src\Franc;
 
 class MoneyTest extends TestCase
 {
@@ -18,5 +17,12 @@ class MoneyTest extends TestCase
     {
         $this->assertTrue( (new Dollar(5))->equals( new Dollar(5) ) );
         $this->assertFalse( (new Dollar(5))->equals( new Dollar(6) ) );
+    }
+
+    public function testFrancMultiplication()
+    {
+        $five = new Franc(5);
+        $this->assertEquals(new Franc(10), $five->times(2));
+        $this->assertEquals(new Franc(15), $five->times(3));
     }
 }
