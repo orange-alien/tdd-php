86ec6052d2abde592110dc94cc3e6fed6e4dc6d5
orange-alien <6963921+orange-alien@users.noreply.github.com>
2023-11-26 05:00:37 +0000

    part1_chapter2_P15

--- test/MoneyTest.php
+++ test/MoneyTest.php
@@ -10,8 +10,10 @@ class MoneyTest extends TestCase
     public function testMultiplication()
     {
         $five = new Dollar(5);
-        $five->times(2);
-        assertEquals(10, $five->amount);
+        $product = $five->times(2);
+        assertEquals(10, $product->amount);
+        $product = $five->times(3);
+        assertEquals(15, $product->amount);
     }
 
 }
