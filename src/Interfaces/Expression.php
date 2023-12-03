3ae3a572da379d2b5b68c484122270540000ef6d
orange-alien <6963921+orange-alien@users.noreply.github.com>
2023-12-03 05:43:15 +0000

    part1_chapter16_P126

--- src/Interfaces/Expression.php
+++ src/Interfaces/Expression.php
@@ -7,6 +7,7 @@ use src\Money;
 
 interface Expression
 {
+    public function times(int $multiplier) : Expression;
     public function plus(Expression $addend) : Expression;
     public function reduce(Bank $bank, string $to) : Money;
 }
--- test/MoneyTest.php
+++ test/MoneyTest.php
@@ -102,6 +102,17 @@ class MoneyTest extends TestCase
         assertEquals(Money::dollar(15), $result);
     }
 
+    public function testMoneyTimes()
+    {
+        $fiveBucks = Money::dollar(5);
+        $tenFrancs = Money::franc(10);
+        $bank = new Bank();
+        $bank->addRate('CHF', 'USD', 2);
+        $sum = ( new Sum($fiveBucks, $tenFrancs) )->times(2);
+        $result = $bank->reduce($sum, 'USD');
+        assertEquals(Money::dollar(20), $result);
+    }
+
 
 
 
